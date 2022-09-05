2d6c0d237b2111ed9a3abd4daf4d1564bba3ee18
gabirosales <gabrielarosales169@gmail.com>
2022-09-05 12:56:57 -0600

    segumiento diseño

---
+++ CRUDSQlite-master/app/src/main/java/com/fjar/app_crudsqlite/Activity_consulta_spinner.java
@@ -0,0 +1,14 @@
+package com.fjar.app_crudsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_consulta_spinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_spinner);
+    }
+}
---
+++ CRUDSQlite-master/app/src/main/java/com/fjar/app_crudsqlite/Activity_detalles_articulos.java
@@ -0,0 +1,14 @@
+package com.fjar.app_crudsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_detalles_articulos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_articulos);
+    }
+}
